63fa398008cb2dcda6bbd87d7296bf20c9fcc6b9
Binh <phuongbinh200429@gmail.com>
2025-02-24 09:45:51 +0700

    feat: first commit

---
+++ rhyza/src/app/shared/components/music-card/music-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-music-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-card.component.html',
+  styleUrl: './music-card.component.scss'
+})
+export class MusicCardComponent {
+
+}
