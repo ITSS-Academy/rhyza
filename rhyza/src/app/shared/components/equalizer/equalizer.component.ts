3cd62b95bdf2e9ebd09a951d9b9ffd96cc938582
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2025-03-10 16:53:52 +0700

    pull code cua duy

---
+++ rhyza/src/app/shared/components/equalizer/equalizer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equalizer',
+  standalone: true,
+  imports: [],
+  templateUrl: './equalizer.component.html',
+  styleUrl: './equalizer.component.scss'
+})
+export class EqualizerComponent {
+
+}
